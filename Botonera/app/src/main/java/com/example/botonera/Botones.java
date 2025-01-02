15ba3ccb898608bf07b5f6ebb9bfba97413757ab
Agustin2211 <72230884+Agustin2211@users.noreply.github.com>
2025-01-02 05:29:33 -0300

    Segunda Vista Empezada

--- Botonera/app/src/main/java/com/example/botonera/Botones.java
+++ Botonera/app/src/main/java/com/example/botonera/Botones.java
@@ -1,6 +1,9 @@
 package com.example.botonera;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,31 @@ public class Botones extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_botones);
+
+        MediaPlayer mpCuandoVosTenesCalle= MediaPlayer.create(this, R.raw.cuando_vos_tenes_calle);
+        MediaPlayer mpDefinicionDeCalle= MediaPlayer.create(this, R.raw.definicion_de_calle);
+        Button buttonCuandoVosTenesCalle = findViewById(R.id.buttonCuandoVosTenesCalle);
+        Button buttonDefinicionDeCalle = findViewById(R.id.buttonDefinicionDeCalle);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        buttonCuandoVosTenesCalle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mpCuandoVosTenesCalle.start();
+            }
+        });
+
+        buttonDefinicionDeCalle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mpDefinicionDeCalle.start();
+            }
         });
     }
 }
