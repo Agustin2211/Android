8d6d055df984fd771da5a0c9f7f79dfce155b126
Agustin2211 <72230884+Agustin2211@users.noreply.github.com>
2025-01-02 20:04:46 -0300

    Ajustes en los botones

--- Botonera/app/src/main/java/com/example/botonera/Botones.java
+++ Botonera/app/src/main/java/com/example/botonera/Botones.java
@@ -19,10 +19,22 @@ public class Botones extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_botones);
 
-        MediaPlayer mpCuandoVosTenesCalle= MediaPlayer.create(this, R.raw.cuando_vos_tenes_calle);
-        MediaPlayer mpDefinicionDeCalle= MediaPlayer.create(this, R.raw.definicion_de_calle);
+        MediaPlayer mpCuandoVosTenesCalle = MediaPlayer.create(this, R.raw.cuando_vos_tenes_calle);
+        MediaPlayer mpDefinicionDeCalle = MediaPlayer.create(this, R.raw.definicion_de_calle);
+        MediaPlayer mpDefinicionDePala = MediaPlayer.create(this, R.raw.definicion_de_pala);
+        MediaPlayer mpSeMalinterpreta = MediaPlayer.create(this, R.raw.se_malinterpreta);
+        MediaPlayer mpTodoParaDecirQue = MediaPlayer.create(this, R.raw.todo_para_decir_que);
+        MediaPlayer mpTodoParaDecirQueTieneContactos = MediaPlayer.create(this, R.raw.todo_para_decir_que_tiene_contactos);
+
+        MediaPlayer mpXD = MediaPlayer.create(this, R.raw.xd);
+
         Button buttonCuandoVosTenesCalle = findViewById(R.id.buttonCuandoVosTenesCalle);
         Button buttonDefinicionDeCalle = findViewById(R.id.buttonDefinicionDeCalle);
+        Button buttonDefinicionDePala = findViewById(R.id.buttonDefinicionDePala);
+        Button buttonSeMalinterpreta = findViewById(R.id.buttonSeMalinterpreta);
+        Button buttonTodoParaDecirQue = findViewById(R.id.buttonTodoParaDecirQue);
+        Button buttonTodoParaDecirQueTieneContactos = findViewById(R.id.buttonTodoParaDecirQueTieneContactos);
+        Button buttonXD = findViewById(R.id.buttonXD);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -42,6 +54,41 @@ public class Botones extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mpDefinicionDeCalle.start();
+            }
+        });
+
+        buttonDefinicionDePala.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mpDefinicionDePala.start();
+            }
+        });
+
+        buttonSeMalinterpreta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mpSeMalinterpreta.start();
+            }
+        });
+
+        buttonTodoParaDecirQue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mpTodoParaDecirQue.start();
+            }
+        });
+
+        buttonTodoParaDecirQueTieneContactos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mpTodoParaDecirQueTieneContactos.start();
+            }
+        });
+
+        buttonXD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mpXD.start();
             }
         });
     }
