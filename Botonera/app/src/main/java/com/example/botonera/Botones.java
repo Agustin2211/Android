55fad1abdbcf318ca97ff91af26f09ccefdfaa84
Agustin2211 <72230884+Agustin2211@users.noreply.github.com>
2025-01-03 18:00:37 -0300

    Animaciones Terminadas

--- Botonera/app/src/main/java/com/example/botonera/Botones.java
+++ Botonera/app/src/main/java/com/example/botonera/Botones.java
@@ -3,7 +3,10 @@ package com.example.botonera;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +16,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Botones extends AppCompatActivity {
 
+    private int contadorBotonesPresionados = 0;
+    private int totalBotones = 7;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_botones);
+
+        Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
 
         MediaPlayer mpCuandoVosTenesCalle = MediaPlayer.create(this, R.raw.cuando_vos_tenes_calle);
         MediaPlayer mpDefinicionDeCalle = MediaPlayer.create(this, R.raw.definicion_de_calle);
@@ -34,7 +42,17 @@ public class Botones extends AppCompatActivity {
         Button buttonSeMalinterpreta = findViewById(R.id.buttonSeMalinterpreta);
         Button buttonTodoParaDecirQue = findViewById(R.id.buttonTodoParaDecirQue);
         Button buttonTodoParaDecirQueTieneContactos = findViewById(R.id.buttonTodoParaDecirQueTieneContactos);
+        Button buttonTodoParaDecirQueLoVienenABuscar = findViewById(R.id.buttonTodoParaDecirQueLoVienenABuscar);
         Button buttonXD = findViewById(R.id.buttonXD);
+
+        TextView tvCuandoVosTenesCalle = findViewById(R.id.tvCuandoVosTenesCalle);
+        TextView tvDefinicionDeCalle = findViewById(R.id.tvDefinicionDeCalle);
+        TextView tvDefinicionDePala = findViewById(R.id.tvDefinicionDePala);
+        TextView tvSeMalinterpreta = findViewById(R.id.tvSeMalinterpreta);
+        TextView tvTodoParaDecirQue = findViewById(R.id.tvTodoParaDecirQue);
+        TextView tvTodoParaDecirQueTienenContactos = findViewById(R.id.tvTodoParaDecirQueTieneContactos);
+        TextView tvTodoParaDecirQueLoVienenABuscar = findViewById(R.id.tvTodoParaDecirQueLoVienenABuscar);
+        TextView tvXD = findViewById(R.id.tvXD);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -47,6 +65,13 @@ public class Botones extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mpCuandoVosTenesCalle.start();
+                tvCuandoVosTenesCalle.startAnimation(fadeInAnimation);
+                tvCuandoVosTenesCalle.setVisibility(View.VISIBLE);
+
+                contadorBotonesPresionados++;
+                if (contadorBotonesPresionados == totalBotones) {
+                    cambiarVisibilidad(tvXD, buttonXD, fadeInAnimation);
+                }
             }
         });
 
@@ -54,6 +79,13 @@ public class Botones extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mpDefinicionDeCalle.start();
+                tvDefinicionDeCalle.startAnimation(fadeInAnimation);
+                tvDefinicionDeCalle.setVisibility(View.VISIBLE);
+
+                contadorBotonesPresionados++;
+                if (contadorBotonesPresionados == totalBotones) {
+                    cambiarVisibilidad(tvXD, buttonXD, fadeInAnimation);
+                }
             }
         });
 
@@ -61,6 +93,13 @@ public class Botones extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mpDefinicionDePala.start();
+                tvDefinicionDePala.startAnimation(fadeInAnimation);
+                tvDefinicionDePala.setVisibility(View.VISIBLE);
+
+                contadorBotonesPresionados++;
+                if (contadorBotonesPresionados == totalBotones) {
+                    cambiarVisibilidad(tvXD, buttonXD, fadeInAnimation);
+                }
             }
         });
 
@@ -68,6 +107,13 @@ public class Botones extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mpSeMalinterpreta.start();
+                tvSeMalinterpreta.startAnimation(fadeInAnimation);
+                tvSeMalinterpreta.setVisibility(View.VISIBLE);
+
+                contadorBotonesPresionados++;
+                if (contadorBotonesPresionados == totalBotones) {
+                    cambiarVisibilidad(tvXD, buttonXD, fadeInAnimation);
+                }
             }
         });
 
@@ -75,6 +121,13 @@ public class Botones extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mpTodoParaDecirQue.start();
+                tvTodoParaDecirQue.startAnimation(fadeInAnimation);
+                tvTodoParaDecirQue.setVisibility(View.VISIBLE);
+
+                contadorBotonesPresionados++;
+                if (contadorBotonesPresionados == totalBotones) {
+                    cambiarVisibilidad(tvXD, buttonXD, fadeInAnimation);
+                }
             }
         });
 
@@ -82,6 +135,26 @@ public class Botones extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mpTodoParaDecirQueTieneContactos.start();
+                tvTodoParaDecirQueTienenContactos.startAnimation(fadeInAnimation);
+                tvTodoParaDecirQueTienenContactos.setVisibility(View.VISIBLE);
+
+                contadorBotonesPresionados++;
+                if (contadorBotonesPresionados == totalBotones) {
+                    cambiarVisibilidad(tvXD, buttonXD, fadeInAnimation);
+                }
+            }
+        });
+
+        buttonTodoParaDecirQueLoVienenABuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvTodoParaDecirQueLoVienenABuscar.startAnimation(fadeInAnimation);
+                tvTodoParaDecirQueLoVienenABuscar.setVisibility(View.VISIBLE);
+
+                contadorBotonesPresionados++;
+                if (contadorBotonesPresionados == totalBotones) {
+                    cambiarVisibilidad(tvXD, buttonXD, fadeInAnimation);
+                }
             }
         });
 
@@ -91,5 +164,12 @@ public class Botones extends AppCompatActivity {
                 mpXD.start();
             }
         });
+    }
+
+    private void cambiarVisibilidad(TextView tvXD, Button buttonXD, Animation fadeInAnimation){
+        tvXD.startAnimation(fadeInAnimation);
+        buttonXD.startAnimation(fadeInAnimation);
+        tvXD.setVisibility(View.VISIBLE);
+        buttonXD.setVisibility(View.VISIBLE);
     }
 }
